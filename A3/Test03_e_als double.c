b451b73dd8fbf3198f73e915d8723b4cf4129012
jfiognri69802eon345vdfkos <44773791+jfiognri69802eon345vdfkos@users.noreply.github.com>
2018-11-12 19:05:59 +0100

    Update Test03_e_als double.c

--- A3/Test03_e_als double.c
+++ A3/Test03_e_als double.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
-double e = 0.0;      /*Variabeln für e*/
-double eold = 0.0;
-int i=0;              /*index*/
-double fspeicher = 1.0;
 
-        do{
-            eold = e;
-            e = (1.0/fspeicher);
-            i ++;
-            fspeicher = fspeicher * i;
-        }while (e!=eold);
-    printf ("e entsprich nach %d schritten angenaehert %f\n", i , e);
-   
-return 0;
+int main(){
+   double e = 0.0;   /*Variabeln für e*/
+   double eold = 0.0;
+   int i = 0;         /*index*/
+   double fspeicher = 1.0;
+
+   do {
+      eold = e;
+      e += (1.0/fspeicher);
+      i++;
+      fspeicher = fspeicher * i;
+   } while (e != eold);
+   printf ("e entsprich nach %d schritten angenaehert %f\n", i, e);
+
+   return 0;
 }
