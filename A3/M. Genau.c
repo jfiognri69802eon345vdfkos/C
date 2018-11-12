df23f561c09e9e686dbd23f85266b76b16027871
Fretze33 <44787796+Fretze33@users.noreply.github.com>
2018-11-12 16:44:49 +0100

    Add files via upload

---
+++ A3/M. Genau.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    double e = 2.0;
+
+    e = 2.0;
+
+    do {
+        e = e / 2;}
+    while (1+(e/2)>1);
+
+        printf("%e", e);
+
+    return (0);
+}
