5ecba9413745d4008f504892ac7f9e7f7abedd6d
jfiognri69802eon345vdfkos <44773791+jfiognri69802eon345vdfkos@users.noreply.github.com>
2018-11-13 21:47:17 +0100

    Add files via upload

--- A4/ImplementierungVonPotenzreihen.c
+++ A4/ImplementierungVonPotenzreihen.c
@@ -4,60 +4,63 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdbool.h>
+double sinus(double);
+double power(double, int16_t);
+uint32_t fakultaet(uint16_t);
 
-uint32_t fakultaet(uint16_t zahl){
-   uint32_t erg = 1;
-   for (uint16_t i = 2; i <= zahl; i++) {
-      erg *= i;
+uint32_t fakultaet(uint16_t zahl){ // Fakultät einer Zahl
+   uint32_t erg = 1; // Variable für das Ergebnis
+   for (uint16_t i = 2; i <= zahl; i++) { // Solange die Laufvariable <= dem Ergebnis ist
+      erg *= i; // Laufvariable mit dem vorherigen Wert multiplizieren.
    }
-   return erg;
+   return erg; // Rückgabe ses Ergebnisses
 }
 
-double power(double basis, int16_t exponent){
-   double erg;
-   bool minus = false;
-   if (exponent != 0) {
+double power(double basis, int16_t exponent){ // basis ^ exponent
+   double erg; // Lokale Variable Ergebnis
+   bool minus = false; // Lokale Variable Vorzeichen
+   if (exponent != 0) { // Falls der Exponent null ist, dann ist das Ergebnis immer 1
       erg = basis;
-      if (exponent < 0) {
+      if (exponent < 0) { // Überprüfen ob der Exponent negativ ist.
          exponent *= (-1);
          minus = true;
       }
       for (int16_t i = 1; i < exponent; i++) {
          erg *= basis;
       }
-      if(minus) {
+      if(minus) { // Hoch eine negative Zahl: 1/(basis ^ exponent)
          erg = 1/erg;
       }
-      return erg;
+      return erg; // Rückgabe des Ergebnisses
    }
-   return 1.0;
+   return 1.0; // Falls Exponent == 0
 }
 
-double sinus(double input){
-   uint32_t k = 0;
-   double erg = 0;
-   double ergvorher = 0;
+double sinus(double input){ // Funktion für die Sinusapproximation
+   uint32_t k = 0; // Variablen deklarieren
+   double erg = 0.0;
+   double ergvorher = 0.0;
    int8_t vorzeichen = -1;
    do {
-      if (vorzeichen > 0) {
+      if (vorzeichen > 0) { // Wechselndes Vorzeichen da jeder zweite Term abgezogen werden soll.
          vorzeichen = -1;
       }else{
          vorzeichen = 1;
       }
-      ergvorher = erg;
-      erg += vorzeichen * (power(input, 2*k + 1)/fakultaet(2*k + 1));
-      k ++;
-      printf("%5.20f\n", erg);
-   } while(ergvorher != erg && k < 30);
-   return erg;
+      ergvorher = erg; // Speichern des vorherigen Wertes von erg
+      erg += vorzeichen * (power(input, 2*k + 1)/fakultaet(2*k + 1)); // Berechnung des Sinus
+      k ++; // Laufvariable einen Schritt weiter setzen
+      printf("%5d -> %e\n", k, erg); // Zwischenergebnis ausgeben
+   } while(ergvorher != erg && k < 30); // Schleife solange sich das Ergebnis ändert.
+   return ergvorher; // Rückgabe des Ergebnisses
 }
 
 int main() {
    // Variablen deklarieren
-   float x = 1.6;
+   float x = 3.14159/2.0;
    printf("Start\n");
 
-   printf("Sinus %f\n", sinus(x));
+   printf("Sinus von %f ist %f\n", x, sinus(x));
 
    printf("\n\nProgramm beendet\n"); // Programmende ausgeben
    return(0);
