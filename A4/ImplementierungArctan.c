6b1b1391483d1085d7e5117322c3e779a242b5d7
jfiognri69802eon345vdfkos <44773791+jfiognri69802eon345vdfkos@users.noreply.github.com>
2018-11-17 22:17:32 +0100

    Add files via upload

--- A4/ImplementierungArctan.c
+++ A4/ImplementierungArctan.c
@@ -6,19 +6,22 @@
 
 int main() {
    // Variablen deklarieren
-   double eingabe = M_PI/8; // Eingabewert
+   double eingabe = M_PI/8; // Eingabewert x ∈ [−1,1]
+   double ergebnis = atan(eingabe);
    double vorherigerWert = eingabe;
    double gesamtErgebnis = vorherigerWert;
-   double gesamtErgebnisvorher = gesamtErgebnis-1;
+   double gesamtErgebnisvorher = gesamtErgebnis - 1;
    printf("Start\n\n");
-
-   for (int i = 1; gesamtErgebnisvorher != gesamtErgebnis; i++) {
-     gesamtErgebnisvorher = gesamtErgebnis;
-     vorherigerWert = (-vorherigerWert)*(eingabe*eingabe);
-     gesamtErgebnis += vorherigerWert/(2*i+1);
-     printf("%3d -> %3.25f\n", i, gesamtErgebnis);
+   printf("Durchlauf   Zwischenergebnis         Abweichung\n");
+   for (int i = 1; gesamtErgebnisvorher != gesamtErgebnis && i < 1000; i++) { // Schleife läuft bis genau genug oder zu lange
+      gesamtErgebnisvorher = gesamtErgebnis; // Update des vorherigen Ergebnisses
+      vorherigerWert = (-vorherigerWert)*(eingabe*eingabe); // VorherigenWert aktualisieren
+      gesamtErgebnis += vorherigerWert/(2*i + 1); // Ergebnisse aufsummieren
+      printf("%3d -> %3.25f - %2.2f %%\n", i, gesamtErgebnis, ((ergebnis - gesamtErgebnis)/ergebnis)*100);
    }
 
+   printf("\n");
+   printf("Das Ergebnis vom Arkustangens von %.5e betr\x84gt %.5e\n", eingabe, gesamtErgebnis);
    printf("\n\nProgramm beendet\n"); // Programmende ausgeben
    return(0);
 }
--- A4/ImplementierungSinus.c
+++ A4/ImplementierungSinus.c
@@ -2,22 +2,26 @@
  * Es wird der Sinuswert eines Punktes durch eine Potenzreihe angenähert.
  */
 #include <stdio.h>
+#include <math.h>
 
 int main() {
    // Variablen deklarieren
-   double eingabe = 3.14159/2.0; // Eingabewert
+   double eingabe = M_PI/2.0; // Eingabewert
+   double ergebnis = sin(eingabe);
    double vorherigerWert = eingabe;
    double gesamtErgebnis = vorherigerWert;
-   double gesamtErgebnisvorher = gesamtErgebnis-1;
+   double gesamtErgebnisvorher = gesamtErgebnis - 1;
    printf("Start\n\n");
-
-   for (int i = 1; gesamtErgebnisvorher != gesamtErgebnis; i++) {
-     gesamtErgebnisvorher = gesamtErgebnis;
-     vorherigerWert = (-vorherigerWert)*(eingabe*eingabe)/((2*i+1)*(2*i));
-     gesamtErgebnis += vorherigerWert;
-     printf("%3d -> %.25f\n", i, gesamtErgebnis);
+   printf("Durchlauf   Zwischenergebnis         Abweichung\n");
+   for (int i = 1; gesamtErgebnisvorher != gesamtErgebnis && i < 1000; i++) { // Schleife läuft bis genau genug oder zu lange
+      gesamtErgebnisvorher = gesamtErgebnis; // Update des vorherigen Ergebnisses
+      vorherigerWert = (-vorherigerWert)*(eingabe*eingabe)/((2*i + 1)*(2*i)); // VorherigenWert aktualisieren
+      gesamtErgebnis += vorherigerWert; // Ergebnisse aufsummieren
+      printf("%3d -> %.25f - %2.2f %%\n", i, gesamtErgebnis, ((ergebnis - gesamtErgebnis)/ergebnis)*100);
    }
 
+   printf("\n");
+   printf("Das Ergebnis vom Sinus von %.5e betr\x84gt %.5e\n", eingabe, gesamtErgebnis);
    printf("\n\nProgramm beendet\n"); // Programmende ausgeben
    return(0);
 }
