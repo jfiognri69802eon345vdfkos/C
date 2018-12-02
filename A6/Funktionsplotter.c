aa9c3c0e9fd21500d72195fb7d5cb526adb6b3bb
jfiognri69802eon345vdfkos <44773791+jfiognri69802eon345vdfkos@users.noreply.github.com>
2018-12-02 17:45:27 +0100

    Update Funktionsplotter.c

--- A6/Funktionsplotter.c
+++ A6/Funktionsplotter.c
@@ -10,40 +10,33 @@ void toMath(double*x, double*y, int minX, int maxX, int minY, int maxY, int widt
    *y = (double)(1)*(minY + (((*y)*(maxY - minY))/height));
 }
 
-void toBMP(double*x, double*y, int minX, int minY, int sizeX, int sizeY, int distX, int distY){
-   if (minX < 0) {
-      *x = (double)(1)*((((*x) + fabs(minX))*(sizeX))/distX);
-   }else{
-      *x = (double)(1)*((((*x) - minX)*(sizeX))/distX);
-   }
-   if (minY < 0) {
-      *y = (double)(1)*((((*y) + fabs(minY))*(sizeY))/distY);
-   }else{
-      *y = (double)(1)*((((*y) - minY)*(sizeY))/distY);
-   }
+void toBMP2(double*x, double*y, int minX, int minY, int maxX, int maxY, int width, int height){
+  *x = (double)(1)*((((*x) - minX)/(maxX - minX))*width);
+  *y = (double)(1)*((((*y) - minY)/(maxY - minY))*height);
 }
 
 int main() {
    printf("Start\n");
-   int maxX = 10;
-   int minX = -10;
-   int maxY = 10;
-   int minY = -10;
-   int distX = 0;
-   int distY = 0;
-   int size = 500; // größe des speicherplatzbedarfs.
+   int maxX = 1;
+   int minX = -1;
+   int maxY = 1;
+   int minY = -1;
+   int size = 100; // größe des speicherplatzbedarfs.
    uint32_t*data;     // var definieren als pointer
-   distX = fabs(minX) + fabs(maxX);
-   distY = fabs(minY) + fabs(maxY);
    data = (uint32_t*) calloc(size * size, sizeof(uint32_t));
    for (int y = 0; y < size; y++) {
       for (int x = 0; x < size; x++) {
          data[y*size + x] = COLOR_WHITE;
       }
    }
+   int t = maxY; // Umdrehen der Y Achse
+   maxY = minY;
+   minY = t;
+   maxY *= (-1);
+   minY *= (-1);
 
    double spalten = 0, zeilen = 0; // Koordinatenachsen
-   toBMP(&spalten, &zeilen, minX, minY, size, size, distX, distY);
+   toBMP2(&spalten, &zeilen, minX, minY, maxX, maxY, size, size);
    int spalten1 = spalten, zeilen1 = zeilen;
    for (int spalte = 0; spalte < size; spalte++) {
       data[zeilen1*size + spalte] = COLOR_RED;
@@ -53,14 +46,13 @@ int main() {
    }
 
    double x = 0, y = 0;
-   for (int xn = 0; xn < size; xn++) {
+   for (int xn = 0; xn < size; xn++) { // Schleife mit der Funktion
       x = xn;
       toMath(&x,&y,minX, maxX, minY, maxY, size, size);
 
       /*
        *  Funktion eingeben
        */
-      // y = sin(x);
       y = x*x;
       /*
        *  Funktion eingabe ende
@@ -68,7 +60,8 @@ int main() {
 
       y *= (-1);
       printf("Rechnung %f - %f\n", x, y);
-      toBMP(&x,&y, minX, minY, size, size, distX, distY);
+      // toBMP(&x,&y, minX, minY, size, size, distX, distY);
+      toBMP2(&x,&y, minX, minY, maxX, maxY, size, size);
       if(y >= 0 && y < size) {
          int xa = x, ya = y;
          data[ya*size + xa] = COLOR_BLACK;
