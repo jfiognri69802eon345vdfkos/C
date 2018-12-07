c4da39a9a43bca8bbdb17297f118997a17be3a9b
Fretze33 <44787796+Fretze33@users.noreply.github.com>
2018-12-07 10:51:34 +0100

    Mandelbrot Versuch

---
+++ A7/Tralla.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include "libBMP.h"
+#include <complex.h>
+#define TT 1000
+
+//void toMath(){
+  //  *x = (double)*((-2)+ (((*x)*(1 - (-2))/width)));
+    //*y = (double)*((1) + (((*y)*((-1) - (1))/height)));
+//}
+//void toBmp2(){
+  //  *x = (double)*((((*x) - (-2))/((1) - (-2)))*width);
+    //*y = (double)*((((*y) - (1))/((-1) - (1)))*height);
+//}
+int Tiefe(int n, double cx, double cy){
+    cx = (cx*cx)+cy;
+
+    if (((cx*cx)+(cy*cy))>=2){
+       return n;}
+    if(n==TT){
+        return (TT);}
+            else{n++;
+              return (Tiefe(n, cx, cy));}
+
+}
+
+int Farben (int zahl){
+if (zahl < 4) {
+     return COLOR_WHITE;
+   } else if (zahl < 10) {
+     return COLOR_GREEN;
+   } else if (zahl < 15) {
+      return COLOR_BLUE;
+   } else if (zahl < 20) {
+ //    return COLOR_YELLOW;
+   //} else if (zahl < 500) {
+     return COLOR_RED;
+   } else {
+      return COLOR_BLACK;
+   }
+   return COLOR_WHITE;
+}
+
+
+int main (){
+
+int width  = 500;
+int hight = 800;
+int n_max  = 100;
+int n = 0;
+double cx,cy;
+int x,y;
+int rn;
+
+uint32_t* pMandel = (uint32_t*) malloc(width*hight*sizeof(uint32_t)); //Weißer Hintergrund warscheinlich unnötig
+for(int j= 0;j<hight;j++){
+        for(int k= 0;k<width;k++){
+    pMandel[j*width+k]=COLOR_WHITE;
+        }
+}
+
+for(int l=0;l<hight;l++){
+        for(int k=0;k<width;k++){
+          n=0;
+          cx=k;
+          cy=l;
+ rn = Tiefe(n, cx, cy);
+ pMandel [l*hight+k]=Farben(rn);
+        }
+}
+//to math();
+bmp_create("pic.bmp", pMandel, width, hight);
+free(pMandel);
+return 0;
+}
