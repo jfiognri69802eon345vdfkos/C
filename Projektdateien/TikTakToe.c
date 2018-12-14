9ef7347a4a58d000950bfce561506cc038c870a2
jfiognri69802eon345vdfkos <44773791+jfiognri69802eon345vdfkos@users.noreply.github.com>
2018-12-14 21:48:51 +0100

    Update TikTakToe.c

--- Projektdateien/TikTakToe.c
+++ Projektdateien/TikTakToe.c
@@ -1,4 +1,4 @@
-//Projekt 
+//Projekt
 
 
 #include <stdio.h>
@@ -12,7 +12,7 @@ static int spielspeicher[3][3] = {{0,0,0},
 
 
 
-static int zug[9] = {1,2,1,2,1,2,1,2,1,};
+static int zug[9] = {1,2,1,2,1,2,1,2,1};
 
 void Feld() {
    printf("\n");
@@ -37,7 +37,7 @@ void Feld() {
 
 
 
-void Spielzug(int z, int **spalte, int  **zeile){
+void Spielzug(int z, int *spalte, int  *zeile){
 
    int n = 1;
    while(n) {
@@ -47,12 +47,15 @@ void Spielzug(int z, int **spalte, int  **zeile){
       printf("Spalte  eingeben\n");
       scanf("%d\n", spalte );
 
-      if (spielspeicher[zeile][spalte] != 0) {
+      if (spielspeicher[*zeile][*spalte] != 0) {
          printf("Feld ist nicht frei");
+      }else{
+        spielspeicher[*zeile][*spalte] = z;
+        n = 0;
       }
-      else {spielspeicher[*zeile][*spalte] = z &&   n = 0;}
    }
 }
+
 int Gewonnen(){
    if((spielspeicher[0][0] == 1) && (spielspeicher[0][1] == 1) && (spielspeicher[0][2] == 1) ||
       (spielspeicher[1][0] == 1) && (spielspeicher[1][1] == 1) && (spielspeicher[1][2] == 1) ||
@@ -78,26 +81,27 @@ int Gewonnen(){
       (spielspeicher[0][0] == 2) && (spielspeicher[1][1] == 2) && (spielspeicher[2][2] == 2) ||
       (spielspeicher[0][2] == 2) && (spielspeicher[1][1] == 2) && (spielspeicher[2][0] == 2)) {
       printf("Spieler 2 hat gewonnen");
+   }else{
+     return 0;
    }
-   else{return 0;}
 }
 
 int main(){
    int n = 1;
-   int zeile;
-   int spalte;
+   int zeile = 0;
+   int spalte = 0;
    int*zp;
    int*sp;
    int eingabe;
    int z = 0;
 
-   zp = %(zeile);
-   sp = %(spalte);
+   zp = &(zeile);
+   sp = &(spalte);
    printf("Das Speiel beginnt");
    while(n) {
       Feld();
       printf("Speiler %d ist am Zug\n", zug[z]);
-      Spielzug(z, &sp, &zp);
+      Spielzug(z, sp, zp);
       z++;
       n = Gewonnen();
    }
