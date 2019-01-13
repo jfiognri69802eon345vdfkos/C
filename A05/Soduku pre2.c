af9939f5f8aca91f80d3bc814978769985f6e705
jfiognri69802eon345vdfkos <44773791+jfiognri69802eon345vdfkos@users.noreply.github.com>
2019-01-13 22:44:16 +0100

    Rename A5/Soduku pre2.c to A05/Soduku pre2.c

--- A05/Soduku pre2.c
+++ A05/Soduku pre2.c
@@ -44,7 +44,7 @@ int pW(int eingabeWert){
 
 
 
-/*static int feldA[9][9] = {{6,3,5,7,2,1,8,4,9},      //Rätsel 1 //static davor schreiben = global lverfügbar
+/*static int feldA[9][9] = {{6,3,5,7,2,1,8,4,9},      //RÃ¤tsel 1 //static davor schreiben = global lverfÃ¼gbar
                     {2,7,8,9,4,5,1,6,3},
                     {9,1,4,6,8,3,7,2,5},
                     {4,9,7,8,5,2,6,3,1},
@@ -54,7 +54,7 @@ int pW(int eingabeWert){
                     {7,6,9,4,3,8,5,1,2},
                     {8,2,3,5,1,9,4,7,6}};*/
 
-static int feldAstart[9][9] =  {{6,3,5,7,2,1,8,4,9},      //Rätsel 1 anfang
+static int feldAstart[9][9] =  {{6,3,5,7,2,1,8,4,9},      //RÃ¤tsel 1 anfang
                             {0,0,8,9,4,5,1,6,3},
                             {0,1,4,6,8,3,7,2,5},
                             {0,9,7,8,0,2,6,3,1},
